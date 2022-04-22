56b680b187461bdba2f284ec6b88c486e867ba19
anhtuan23 <dotuananh.dta@gmail.com>
2022-04-22 08:37:26 +0700

    add some consts

--- const.py
+++ const.py
@@ -1,12 +1,16 @@
+ACC1_NAME = "acc1"
+ACC2_NAME = "acc2"
 ACC_USER_DICT = {
-    "acc1": ["ta", "tn"],
-    "acc2": ["tl", "la"],
+    ACC1_NAME: ["ta", "tn"],
+    ACC2_NAME: ["tl", "la"],
 }
 
 ACC_NAME_L = list(ACC_USER_DICT.keys())
 ACC_COMBINED_NAME = "acc_combined"
 ALL_ACC_NAME_L = ACC_NAME_L + [ACC_COMBINED_NAME]
 
-INDEX_NAME_L = ["vni", "vn30"]
+VNI_NAME = "vni"
+VN30_NAME = "vn30"
+INDEX_NAME_L = [VNI_NAME, VN30_NAME]
 INDEX_COMBINED_NAME = "index_combined"
 ALL_INDEX_NAME_L = INDEX_NAME_L + [INDEX_COMBINED_NAME]
